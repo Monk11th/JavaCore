111526caa1224b931b3bb025f483e989b925e0bc
Monk11th <roman.portnykh@gmail.com>
2021-08-18 22:26:55 +0300

    HW4

--- src/HWs/HW4/PhoneNo.java
+++ src/HWs/HW4/PhoneNo.java
@@ -1,2 +1,23 @@
-package HWs.HW4;public class PhoneNo {
+package HWs.HW4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneNo {
+    private static final Map<String, List<String>> phoneNo = new HashMap<>();
+
+    public static void add(String name, String... phonesAdd) {
+        if (phoneNo.get(name) != null) {
+            List<String> phones = new ArrayList<>(phoneNo.get(name));
+            phones.addAll(List.of(phonesAdd));
+            phoneNo.put(name, phones);
+        } else {
+            phoneNo.put(name, List.of(phonesAdd));
+        }
+    }
+    public static void get(String name) {
+        System.out.printf("%s: %s\n", name, phoneNo.get(name));
+    }
 }
